ad9d180882641da2a951692e21a8914b657d7e7e
Vitor Fel <vitorfel2000@gmail.com>
2024-08-15 22:19:02 -0300

    :tada: Configuracao das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const APP_ROUTES: Routes = [];
+export const APP_ROUTES: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./pages/pages.routes').then(m => m.PAGES_ROUTES),
+    },
+];
---
+++ src/app/shared/components/teste/teste.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  standalone: true,
+  imports: [],
+  templateUrl: './teste.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TesteComponent {
+
+}
