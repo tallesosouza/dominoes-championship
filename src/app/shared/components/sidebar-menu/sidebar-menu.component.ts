f709dd53f9d177bebd60316e3d21eddf3db97d51
Vitor Fel <vitorfel2000@gmail.com>
2024-08-19 20:50:41 -0300

    :wrench: Paths

---
+++ src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-menu.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SidebarMenuComponent {
+
+}
