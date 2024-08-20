afa1434492a78ad81f47f582ca8328388bdefcad
Vitor Fel <vitorfel2000@gmail.com>
2024-08-19 23:16:16 -0300

    :construction: Sidebar componente

--- src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,13 +1,39 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TooltipModule } from 'primeng/tooltip';
+import { SkeletonModule } from 'primeng/skeleton';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import type { MenuInterface } from '@core/interfaces/menu';
+import { MENU_LIST } from '@core/const/menu-list';
 
 @Component({
-  selector: 'app-sidebar-menu',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar-menu.component.html',
-  styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-sidebar-menu',
+	standalone: true,
+	imports: [NgClass, TooltipModule, SkeletonModule, RouterLink, RouterLinkActive],
+	templateUrl: './sidebar-menu.component.html',
+	styleUrl: './sidebar-menu.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	animations: [
+		trigger('toogleMenuInternal', [
+			transition(':enter', [style({ opacity: 0 }), animate('500ms', style({ opacity: 1 }))]),
+			transition(':leave', [animate('100ms', style({ opacity: 0 }))]),
+		]),
+	],
 })
 export class SidebarMenuComponent {
+	protected openMenu = signal(false);
+	protected menuList = signal<MenuInterface[]>(MENU_LIST);
 
+	protected tooltipMenuText = computed(() => {
+		if (this.openMenu()) {
+			return 'Fechar menu';
+		}
+
+		return 'Abrir menu';
+	});
+
+	protected clickToggleMenu() {
+		this.openMenu.set(!this.openMenu());
+	}
 }
