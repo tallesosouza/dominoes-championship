75cf412196ef4de66d19ebe9818ef278c3d5a1fa
Vitor Fel <vitorfel2000@gmail.com>
2024-08-19 21:22:11 -0300

    :heavy_plus_sign: Provider animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(APP_ROUTES)],
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(APP_ROUTES),
+		provideAnimationsAsync(),
+	],
 };
