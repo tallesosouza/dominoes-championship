c8af8a1237bf1684a7ddead4f59552522a303f5b
Vitor Fel <vitorfel2000@gmail.com>
2024-08-20 23:06:45 -0300

    :construction: Criando componente image de perfil

---
+++ src/app/shared/components/profile-image/profile-image.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+
+@Component({
+	selector: 'app-profile-image',
+	standalone: true,
+	imports: [],
+	template: `
+    @if (src()) {
+      <img
+        [class]="getSize() + ' border-circle shadow-5'"
+        [src]="src()"
+        [alt]="alt()"
+      />
+    } @else {
+      <div
+        [class]="getSize() +
+        ' text-4xl surface-100 border-circle shadow-5 flex align-items-center justify-content-center'"
+      >
+        <i class="fa fa-user"></i>
+      </div>
+    }
+  `,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProfileImageComponent {
+	public src = input('');
+	public alt = input('');
+	public size = input<'small' | 'medium' | 'large'>('small');
+
+	public getSize = computed(() => {
+		switch (this.size()) {
+			case 'large':
+				return 'w-5rem h-5rem';
+			case 'medium':
+				return 'w-4rem h-4rem';
+			case 'small':
+				return 'w-2rem h-2rem';
+		}
+	});
+}
