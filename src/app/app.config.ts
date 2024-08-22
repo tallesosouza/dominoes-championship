9c2725c10ccf2714d47ff05d5d38714cf4789027
Vitor Fel <vitorfel2000@gmail.com>
2024-08-22 17:29:50 -0300

    :wrench: Adicionando configuracao no app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { APP_ROUTES } from './app.routes';
+import {
+	type ApplicationConfig,
+	DEFAULT_CURRENCY_CODE,
+	LOCALE_ID,
+	provideZoneChangeDetection,
+} from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { DialogService } from 'primeng/dynamicdialog';
+import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(APP_ROUTES),
 		provideAnimationsAsync(),
+		{ provide: LOCALE_ID, useValue: 'pt' },
+		{ provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+		DialogService,
 	],
 };
