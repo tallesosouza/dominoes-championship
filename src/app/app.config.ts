ba92dbb36c5e2ffa85426a6b375d6a3d7a82d845
Vitor Fel <vitorfel2000@gmail.com>
2024-08-23 15:33:24 -0300

    :wrench: Configurando http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,12 +9,14 @@ import { provideRouter } from '@angular/router';
 import { DialogService } from 'primeng/dynamicdialog';
 import { APP_ROUTES } from './app.routes';
 import { MessageService } from 'primeng/api';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(APP_ROUTES),
 		provideAnimationsAsync(),
+		provideHttpClient(),
 		{ provide: LOCALE_ID, useValue: 'pt' },
 		{ provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
 		DialogService,
