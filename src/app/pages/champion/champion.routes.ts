c4b49e498eefe64763b5fecc5c6011d287627a21
Vitor Fel <vitorfel2000@gmail.com>
2024-08-25 16:37:32 -0300

    :sparkles: Definindo as rotas de listagem e cadastro do campeonato

--- src/app/pages/champion/champion.routes.ts
+++ src/app/pages/champion/champion.routes.ts
@@ -1,17 +1,19 @@
 import type { Routes } from '@angular/router';
+import { ChampionFormComponent } from './champion-form/champion-form.component';
+import { ChampionListComponent } from './champion-list/champion-list.component';
 import { ChampionComponent } from './champion.component';
 
 export const CHAMPION_ROUTES: Routes = [
 	{
 		path: '',
+		component: ChampionListComponent,
+	},
+	{
+		path: 'new',
+		component: ChampionFormComponent,
+	},
+	{
+		path: 'progress/:uuid',
 		component: ChampionComponent,
 	},
-	// {
-	// 	path: 'new',
-	// 	component: UserFormComponent,
-	// },
-	// {
-	// 	path: 'edit/:uuid',
-	// 	component: UserFormComponent,
-	// },
 ];
