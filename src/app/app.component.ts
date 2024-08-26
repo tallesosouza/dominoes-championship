2b9a271e2be950499684e4603daa8aa8d239aead
Vitor Fel <vitorfel2000@gmail.com>
2024-08-26 15:44:17 -0300

    :sparkles: Iniciando service do torneio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, type OnInit } from '@angular/core';
+import { Component, type OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChampionStorageService } from '@core/services/champion-storage.service';
 import { UserStorageService } from '@core/services/user-storage.service';
 import { ToastComponent } from '@shared/components/toast/toast.component';
 
@@ -14,8 +15,10 @@ import { ToastComponent } from '@shared/components/toast/toast.component';
 })
 export class AppComponent implements OnInit {
 	private userStorage = inject(UserStorageService);
+	private championStorage = inject(ChampionStorageService);
 
 	ngOnInit(): void {
 		this.userStorage.init();
+		this.championStorage.init();
 	}
 }
