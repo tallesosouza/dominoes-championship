f88195050c8a7766bf57d3a700b0c5a409799f06
Vitor Fel <vitorfel2000@gmail.com>
2024-08-26 15:43:28 -0300

    :label: Inserindo fases do torneio

--- src/app/core/interfaces/champion.ts
+++ src/app/core/interfaces/champion.ts
@@ -1,5 +1,18 @@
+import type { UserInterface } from './user';
+
 export interface ChampionInterface {
 	uuid: string;
 	title: string;
 	description?: string;
+	tournamentPhase:
+		| 'FIRST_PHASE'
+		| 'SECOND_PHASE'
+		| 'THIRD_PHASE'
+		| 'FOURTH_PHASE'
+		| 'FIFTH_PHASE'
+		| 'SIXTH_PHASE'
+		| 'SEVENTH_PHASE'
+		| 'SEMI_FINAL'
+		| 'FINAL';
+	players: Array<UserInterface & { status: 'ELIMINATED' | 'IN_PROGRESS' | 'CLASSIFIED' }>;
 }
