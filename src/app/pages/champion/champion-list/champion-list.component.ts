12aed60b9fffb6093d1be1a255cc50728d78f6b6
Vitor Fel <vitorfel2000@gmail.com>
2024-08-26 15:44:49 -0300

    :sparkles: Integrando lista do torneio

--- src/app/pages/champion/champion-list/champion-list.component.ts
+++ src/app/pages/champion/champion-list/champion-list.component.ts
@@ -1,7 +1,16 @@
-import { ChangeDetectionStrategy, Component, type OnInit, signal } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	DestroyRef,
+	type OnInit,
+	inject,
+	signal,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import type { ChampionInterface } from '@core/interfaces/champion';
+import { ChampionStorageService } from '@core/services/champion-storage.service';
 import { ChampionCardComponent } from '@shared/components/champion-card/champion-card.component';
 import { MainHeaderComponent } from '@shared/components/main-header/main-header.component';
 import { ButtonDirective } from 'primeng/button';
@@ -9,6 +18,7 @@ import { CardModule } from 'primeng/card';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
+import { debounceTime, take } from 'rxjs';
 @Component({
 	selector: 'app-champion-list',
 	standalone: true,
@@ -27,10 +37,32 @@ import { InputTextModule } from 'primeng/inputtext';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChampionListComponent implements OnInit {
+	private championStorage = inject(ChampionStorageService);
+	private destroy = inject(DestroyRef);
+
 	protected searchControl = new FormControl();
 	protected gridData = signal<ChampionInterface[]>([]);
 
 	ngOnInit(): void {
-		this.searchControl.disable();
+		this.getChampionList();
+		this.searchControlObservable();
+	}
+
+	private getChampionList() {
+		this.championStorage
+			.get()
+			.pipe(take(1))
+			.subscribe((res) => {
+				this.gridData.set(res);
+			});
+	}
+
+	private searchControlObservable() {
+		this.searchControl.valueChanges
+			.pipe(debounceTime(300), takeUntilDestroyed(this.destroy))
+			.subscribe((value) => {
+				const dto = this.championStorage.getByTitleAndDescription(value);
+				this.gridData.update((_) => dto);
+			});
 	}
 }
