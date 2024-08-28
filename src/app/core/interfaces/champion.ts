4388a085d06022f966179ff2415215b513d8d709
Vitor Fel <vitorfel2000@gmail.com>
2024-08-27 22:36:21 -0300

    :label: Inserindo tipagem da primeira etapa

--- src/app/core/interfaces/champion.ts
+++ src/app/core/interfaces/champion.ts
@@ -4,15 +4,32 @@ export interface ChampionInterface {
 	uuid: string;
 	title: string;
 	description?: string;
-	tournamentPhase:
-		| 'FIRST_PHASE'
-		| 'SECOND_PHASE'
-		| 'THIRD_PHASE'
-		| 'FOURTH_PHASE'
-		| 'FIFTH_PHASE'
-		| 'SIXTH_PHASE'
-		| 'SEVENTH_PHASE'
-		| 'SEMI_FINAL'
-		| 'FINAL';
-	players: Array<UserInterface & { status: 'ELIMINATED' | 'IN_PROGRESS' | 'CLASSIFIED' }>;
+	tournamentPhase: TournamentPhaseDto;
+	players: PlayerInterface[];
+	stages: StagesInterface;
+	stage: number;
 }
+
+export type PlayerInterface = UserInterface & {
+	status: 'ELIMINATED' | 'IN_PROGRESS' | 'CLASSIFIED';
+};
+
+export type TournamentPhaseDto =
+	| 'FIRST_PHASE'
+	| 'SECOND_PHASE'
+	| 'THIRD_PHASE'
+	| 'FOURTH_PHASE'
+	| 'FIFTH_PHASE'
+	| 'SIXTH_PHASE'
+	| 'SEVENTH_PHASE'
+	| 'SEMI_FINAL'
+	| 'FINAL';
+
+export interface StagesInterface {
+	firstPhase: TablesInterface;
+}
+
+export type TablesInterface = {
+	tables: Array<PlayerInterface[]>;
+	status: 'START' | 'IN_PROGRESS' | 'FINALIZED';
+};
