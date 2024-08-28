466bbf0e8dd609a367b70abae02df1b0c8f39f0b
Vitor Fel <vitorfel2000@gmail.com>
2024-08-27 22:38:55 -0300

    :sparkles: Inserindo componente do load na pagina

--- src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,17 +1,28 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MENU_LIST } from '@core/const/menu-list';
 import type { MenuInterface } from '@core/interfaces/menu';
+import { LoadingService } from '@core/services/loading.service';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TooltipModule } from 'primeng/tooltip';
 import { FooterComponent } from '../footer/footer.component';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
 	selector: 'app-sidebar-menu',
 	standalone: true,
-	imports: [NgClass, TooltipModule, SkeletonModule, RouterLink, RouterLinkActive, FooterComponent],
+	imports: [
+		NgClass,
+		TooltipModule,
+		SkeletonModule,
+		RouterLink,
+		RouterLinkActive,
+		FooterComponent,
+		LoadingComponent,
+	],
 	templateUrl: './sidebar-menu.component.html',
 	styleUrl: './sidebar-menu.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -23,8 +34,11 @@ import { FooterComponent } from '../footer/footer.component';
 	],
 })
 export class SidebarMenuComponent {
+	private loadingService = inject(LoadingService);
+
 	protected openMenu = signal(false);
 	protected menuList = signal<MenuInterface[]>(MENU_LIST);
+	protected loading = toSignal(this.loadingService.get());
 
 	protected tooltipMenuText = computed(() => {
 		if (this.openMenu()) {
