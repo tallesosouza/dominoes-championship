8dd41d62f86a8d0c829005b1825d3664e37efa8e
Vitor Fel <vitorfel2000@gmail.com>
2024-08-27 22:37:24 -0300

    :sparkles: Criando service do loading da pagina

---
+++ src/app/core/services/loading.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class LoadingService {
+	private store$ = new BehaviorSubject(false);
+
+	public get() {
+		return this.store$.asObservable();
+	}
+
+	public set(value: boolean) {
+		this.store$.next(value);
+	}
+
+	public setToggle() {
+		this.set(true);
+
+		setTimeout(() => {
+			this.set(false);
+		}, 1500);
+	}
+}
