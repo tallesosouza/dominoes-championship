96326d7cdf373c8ee600f8035040a9ec6039407d
Vitor Fel <vitorfel2000@gmail.com>
2024-08-27 22:35:56 -0300

    :wrench: Configuracao da primeira etapa do torneio

---
+++ src/app/shared/helpers/champion-config.ts
@@ -0,0 +1,14 @@
+import type { TablesInterface } from '@core/interfaces/champion';
+
+export const FIRST_PHASE_TABLES_QUANT = 8;
+
+export function isFirstPhaseValid(data: TablesInterface) {
+	const eliminatedQuant = data.tables.flatMap((res) =>
+		res.filter((res) => res.status === 'ELIMINATED'),
+	);
+
+	if (eliminatedQuant.length === FIRST_PHASE_TABLES_QUANT) {
+		return true;
+	}
+	return false;
+}
