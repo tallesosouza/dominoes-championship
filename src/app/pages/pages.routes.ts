29e9120232aca07bb311ae60ab0ec3ef44bc1fad
Vitor Fel <vitorfel2000@gmail.com>
2024-09-04 20:31:16 -0300

    :boom: Removendo loadChildren

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,5 +1,10 @@
 import type { Routes } from '@angular/router';
+import { ChampionFormComponent } from './champion/champion-form/champion-form.component';
+import { ChampionListComponent } from './champion/champion-list/champion-list.component';
+import { ChampionProgressComponent } from './champion/champion-progress/champion-progress.component';
 import { PagesComponent } from './pages.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
 export const PAGES_ROUTES: Routes = [
 	{
@@ -8,11 +13,39 @@ export const PAGES_ROUTES: Routes = [
 		children: [
 			{
 				path: 'user',
-				loadChildren: () => import('./user/user.routes').then((m) => m.USER_ROUTES),
+				children: [
+					{
+						path: '',
+						component: UserListComponent,
+					},
+					{
+						path: 'new',
+						component: UserFormComponent,
+					},
+					{
+						path: 'edit/:uuid',
+						component: UserFormComponent,
+					},
+				],
+				// loadChildren: () => import('./user/user.routes').then((m) => m.USER_ROUTES),
 			},
 			{
 				path: 'champion',
-				loadChildren: () => import('./champion/champion.routes').then((m) => m.CHAMPION_ROUTES),
+				children: [
+					{
+						path: '',
+						component: ChampionListComponent,
+					},
+					{
+						path: 'new',
+						component: ChampionFormComponent,
+					},
+					{
+						path: 'progress/:uuid',
+						component: ChampionProgressComponent,
+					},
+				],
+				// loadChildren: () => import('./champion/champion.routes').then((m) => m.CHAMPION_ROUTES),
 			},
 		],
 	},
