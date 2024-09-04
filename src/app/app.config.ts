7a9eafcb090ddce8d72f02ac65996ad8f312a3c0
Vitor Fel <vitorfel2000@gmail.com>
2024-09-04 20:13:33 -0300

    :package: PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import {
 	type ApplicationConfig,
 	DEFAULT_CURRENCY_CODE,
 	LOCALE_ID,
+	isDevMode,
 	provideZoneChangeDetection,
 } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import { MessageService } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 import { APP_ROUTES } from './app.routes';
@@ -21,5 +23,9 @@ export const appConfig: ApplicationConfig = {
 		{ provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
 		DialogService,
 		MessageService,
+		provideServiceWorker('ngsw-worker.js', {
+			enabled: !isDevMode(),
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
 	],
 };
